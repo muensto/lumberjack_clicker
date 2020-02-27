d8743412c92fc21db6b05e325a4d9dc1041d883a
tomuit01 <tomuit01@hs-esslingen.de>
2020-02-27 22:39:01 +0100

    fastest bot so far. c :*

--- lumberjack_clicker.py
+++ lumberjack_clicker.py
@@ -1,6 +1,19 @@
 from PIL import ImageGrab
 import time, pyautogui, random
+from ctypes import windll
 
+
+def get_color(rgb):
+    r = rgb & 0xff
+    g = (rgb >> 8) & 0xff
+    b = (rgb >> 16) & 0xff
+    return r, g, b
+
+
+def get_pixel(x, y):  # Modify class atribute
+    screen = windll.user32.GetDC(0)
+    rgb = windll.gdi32.GetPixel(screen, x, y)
+    return get_color(rgb)
 
 def Pixel(X, Y, image):
     return image.getpixel((X, Y))
@@ -17,17 +30,26 @@ def Lumberjack():
     time.sleep(2)
     pyautogui.press(' ')
 
+
     while Score < scoreX:
 
-        image = ImageGrab.grab(image_bbox)
+        # image = ImageGrab.grab(image_bbox)
+        #
+        # Left = Pixel(0, 0, image)
+        # Right = Pixel(image._size[0] - 1, image._size[1] - 1, image)
+        pixel = get_pixel(449, 633)
 
-        Left = Pixel(0, 0, image)
-        Right = Pixel(image._size[0] - 1, image._size[1] - 1, image)
+        if pixel == (161, 116, 56):
+            pyautogui.press('right', 2, 0.07)
 
-        if Right[0] < Left[0]:
-            pyautogui.press('left', 2, 0.09)
-        else:
-            pyautogui.press('right', 2, 0.09)
+        elif pixel == (211, 247, 255) or pixel == (241, 252, 255):
+            pyautogui.press('left', 2, 0.07)
+        # if Right[0] < Left[0]:
+        #     pyautogui.press('left', 2, 0.09)
+        #
+        # else:
+        #     pyautogui.press('right', 2, 0.09)
+
         Score += 2
 
 
